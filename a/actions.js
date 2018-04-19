33ef35a9101cae93a25dd649b331243a64b7bdd9
cjh <1418503647@qq.com>
2018-04-20 01:10:36 +0800

    update

--- a/actions.js
+++ a/actions.js
@@ -1,5 +1,45 @@
 let ACTIONS = [
     {
+        title: '文本编辑器',
+        description: '',
+        type: 'text/plain',
+        icon: 'https://www.baidu.com/favicon.ico',
+        url: 'https://text.yunser.com/editor',
+        domain: 'https://text.yunser.com'
+    },
+    {
+        title: '文本处理',
+        description: '',
+        type: 'text/plain',
+        icon: 'https://www.baidu.com/favicon.ico',
+        url: 'https://text.yunser.com/tool',
+        domain: 'https://text.yunser.com'
+    },
+    {
+        title: '保存到我的云盘',
+        description: '',
+        type: 'text/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        url: 'https://file.yunser.com/',
+        domain: 'https://file.yunser.com'
+    },
+    {
+        title: '保存到我的云盘',
+        description: '',
+        type: 'image/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        url: 'https://file.yunser.com/',
+        domain: 'https://file.yunser.com'
+    },
+    {
+        title: '选择文件',
+        description: '',
+        type: '*/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        url: 'https://file.yunser.com/',
+        domain: 'https://file.yunser.com'
+    },
+    {
         type: 'text/plain',
         icon: 'https://www.baidu.com/favicon.ico',
         title: '最简单的文本编辑器',
@@ -11,9 +51,22 @@ let ACTIONS = [
         type: 'image/*',
         icon: 'https://www.baidu.com/favicon.ico',
         title: '图片浏览器',
-        url: 'https://b.yunser.com/image.html',
-        disposition: 'inline',
-        domain: 'https://b.yunser.com'
+        url: 'https://imageviewer.yunser.com/',
+        domain: 'https://imageviewer.yunser.com'
+    },
+    {
+        type: 'image/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        title: '修改图片大小',
+        url: 'https://imagetool.yunser.com/size',
+        domain: 'https://imagetool.yunser.com'
+    },
+    {
+        type: 'image/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        title: '马赛克工具',
+        url: 'https://imagetool.yunser.com/mosaic',
+        domain: 'https://imagetool.yunser.com'
     },
     {
         type: 'text/plain',
@@ -28,17 +81,37 @@ let ACTIONS = [
         icon: 'https://www.baidu.com/favicon.ico',
         title: '文本阅读器',
         url: 'https://reader.yunser.com',
-        disposition: 'inline',
         domain: 'https://reader.yunser.com'
+    },
+    {
+        type: 'text/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        title: 'Markdown 阅读器',
+        url: 'https://reader.yunser.com',
+        domain: 'https://reader.yunser.com'
+    },
+    {
+        type: 'text/*',
+        icon: 'https://www.baidu.com/favicon.ico',
+        title: 'Markdown 编辑器',
+        url: 'https://mdeditor.yunser.com',
+        domain: 'https://mdeditor.yunser.com'
     },
     {
         type: 'image/*',
         icon: 'https://www.baidu.com/favicon.ico',
         title: '图片选择器',
-        url: 'https://d.yunser.com',
-        disposition: 'inline',
-        domain: 'https://reader.yunser.com'
+        url: 'https://image.yunser.com/picker',
+        domain: 'https://image.yunser.com'
     },
+    // {
+    //     type: 'image/*',
+    //     icon: 'https://www.baidu.com/favicon.ico',
+    //     title: '图片选择器',
+    //     url: 'https://d.yunser.com',
+    //     disposition: 'inline',
+    //     domain: 'https://reader.yunser.com'
+    // },
     {
         type: 'text/*',
         icon: 'https://www.baidu.com/favicon.ico',
